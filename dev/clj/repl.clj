9cb80492833204dd9c2b79217af55767ba89b0d4
Joel Sánchez <webmaster@kazer.es>
2019-02-23 15:13:54 +0100

    Get the repl ns up to date

--- dev/clj/repl.clj
+++ dev/clj/repl.clj
@@ -1,20 +1,49 @@
 (ns repl
   (:require
    [clojure.tools.namespace.repl :as tn]
-   [ventas-devtools.repl :as devtools.repl]
-   [clojure.repl :refer :all]
-   [clojure.core.async :as core.async :refer [<! chan >! go]]
-   [ventas.server]
-   [clojure.spec.alpha :as spec]
-   [ventas.server.spa]))
+   [ventas.server :as server]
+   [ventas.system :as system]
+   [mount.core :as mount]
+   [ventas.themes.clothing.core :as core]
+   [shadow.cljs.devtools.server :as shadow.server]
+   [shadow.cljs.devtools.api :as shadow.api]))
 
-(def cljs-repl        devtools.repl/cljs-repl)
-(def r                devtools.repl/r)
-(def run-tests        devtools.repl/run-tests)
-(def set-themes!      devtools.repl/set-themes!)
-(def tn-refresh       tn/refresh)
+(defn refresh [& {:keys [after]}]
+  (let [result (tn/refresh :after after)]
+    (when (instance? Throwable result)
+      (throw result))))
+
+(defn refresh-all [& {:keys [after]}]
+  (let [result (tn/refresh-all :after after)]
+    (when (instance? Throwable result)
+      (throw result))))
+
+(alter-var-root #'*warn-on-reflection* (constantly true))
+(tn/set-refresh-dirs "dev/clj" "src/clj" "src/cljc")
+
+(defn start [& [states]]
+  (-> (mount/only (or states system/default-states))
+      (mount/with-args {::server/handler #'core/handler})
+      mount/start)
+  :done)
+
+(defn r [& subsystems]
+  (let [states (system/get-states subsystems)]
+    (when (seq states)
+      (mount/stop states))
+    (refresh :after 'repl/start)))
+
+(defn init-next []
+  (start))
 
 (defn init []
-  (devtools.repl/init)
-  (set-themes! #{:clothing :admin}))
+  (require 'ventas.core)
+  (refresh-all :after 'repl/init-next))
 
+(defn watch-cljs [build-id]
+  (shadow.server/start!)
+  (shadow.api/watch build-id))
+
+(defn release-cljs [build-id]
+  (shadow.server/start!)
+  (shadow.api/release build-id))
