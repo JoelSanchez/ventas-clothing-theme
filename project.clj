8783b657294ab656345a9d91d77100e5df066811
Joel Sánchez <webmaster@kazer.es>
2019-04-18 17:20:45 +0200

    Version 0.1.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ventas-clothing-theme "0.1.4"
+(defproject ventas-clothing-theme "0.1.5-SNAPSHOT"
   :description "The Clothing ventas theme. Intended for clothing stores."
 
   :url "https://github.com/joelsanchez/ventas-clothing-theme"
